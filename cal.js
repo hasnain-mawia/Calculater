4a86929a3d3adbe61ba6629741a34e796bb03ccc
hasnain-mawia <110049696+hasnain-mawia@users.noreply.github.com>
2023-08-02 14:43:23 +0500

    cal.js

--- cal.js
+++ cal.js
@@ -18,30 +18,3 @@
     result.value = result.value.slice(0,-1);
 
 }
-
-
-
-    // let notification = document.getElementById('result');
-    // notification.style.display = "none"
-
-
-
-
-
-
-// let value_1 = document.getElementById('value-1');
-// let value_2 = document.getElementById('value-2');
-
-// let display = () => {
-//     // if (value_1 == "" && value_2 == "") {
-//     //     alert('Please Enter the value')
-//     // }
-//      let values = (`${value_1.value}${value_2.value}`);
-//     console.log(values)
-//     notification.style.display = "block"
-//     value_1.value = "";
-//     value_2.value = "";
-}
-
-
-
